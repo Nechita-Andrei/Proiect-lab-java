3f4627024488d9a33f068297ef0b6008351edcd1
Busuioc-Andrei <andreitudor23@gmail.com>
2023-01-21 21:30:21 +0200

    Fixed Zbor model

--- src/main/java/proiect/domain/Zbor.java
+++ src/main/java/proiect/domain/Zbor.java
@@ -17,53 +17,97 @@ public class Zbor {
     @Column(name = "id_zbor")
     private Integer id;
 
-
-//
     @ManyToMany(fetch = FetchType.LAZY)
     @JsonIgnore
     @JoinTable(name = "bilet", joinColumns = @JoinColumn(name = "id_zbor"), inverseJoinColumns = @JoinColumn(name = "id_client"))
     private Set<Client> pasageri;
-    public Zbor() {
 
+    @ManyToOne
+    @JoinColumn(name = "id_aeroport_plecare",referencedColumnName = "id_aeroport")
+    private Aeroport aeroport_plecare;
+
+    @ManyToOne
+    @JoinColumn(name = "id_aeroport_sosire",referencedColumnName = "id_aeroport")
+    private Aeroport aeroport_sosire;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pilot",referencedColumnName = "id_pilot")
+    private Pilot pilot;
+
+    @ManyToOne
+    @JoinColumn(name = "id_copilot",referencedColumnName = "id_pilot")
+    private Pilot copilot;
+
+    @ManyToOne
+    @JoinColumn(name = "id_companie_aeriana",referencedColumnName = "id_companie_aeriana")
+    private CompanieAeriana companie_aeriana;
+
+    @ManyToOne
+    @JoinColumn(name = "id_avion",referencedColumnName = "id_avion")
+    private Avion avion;
+
+    @Column(nullable = false)
+    private Date data_plecare_estimata;
+
+    @Column(nullable = false)
+    private Date data_sosire_estimata;
+
+    private Date data_plecare_reala;
+
+    private Date data_sosire_reala;
+
+    public void addPasager(Client client){
+        pasageri.add(client);
     }
 
-    @Column
-    private int delay;
+    public Zbor(Integer id, Set<Client> pasageri, Aeroport aeroport_plecare, Aeroport aeroport_sosire, Pilot pilot, Pilot copilot, CompanieAeriana companie_aeriana, Avion avion, Date data_plecare_estimata, Date data_sosire_estimata, Date data_plecare_reala, Date data_sosire_reala) {
+        this.id = id;
+        this.pasageri = pasageri;
+        this.aeroport_plecare = aeroport_plecare;
+        this.aeroport_sosire = aeroport_sosire;
+        this.pilot = pilot;
+        this.copilot = copilot;
+        this.companie_aeriana = companie_aeriana;
+        this.avion = avion;
+        this.data_plecare_estimata = data_plecare_estimata;
+        this.data_sosire_estimata = data_sosire_estimata;
+        this.data_plecare_reala = data_plecare_reala;
+        this.data_sosire_reala = data_sosire_reala;
+    }
 
-    public int getId() {
+    public Zbor() {
+    }
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public void setPret(int pret) {
-        this.pret = pret;
+    public Set<Client> getPasageri() {
+        return pasageri;
     }
 
-    public Aeroport getPlecare() {
-        return plecare;
+    public void setPasageri(Set<Client> pasageri) {
+        this.pasageri = pasageri;
     }
 
-    public void setPlecare(Aeroport plecare) {
-        this.plecare = plecare;
+    public Aeroport getAeroport_plecare() {
+        return aeroport_plecare;
     }
 
-    public Aeroport getSosire() {
-        return sosire;
+    public void setAeroport_plecare(Aeroport aeroport_plecare) {
+        this.aeroport_plecare = aeroport_plecare;
     }
 
-    public void setSosire(Aeroport sosire) {
-        this.sosire = sosire;
+    public Aeroport getAeroport_sosire() {
+        return aeroport_sosire;
     }
 
-    public Avion getAvion() {
-        return avion;
-    }
-
-    public void setAvion(Avion avion) {
-        this.avion = avion;
+    public void setAeroport_sosire(Aeroport aeroport_sosire) {
+        this.aeroport_sosire = aeroport_sosire;
     }
 
     public Pilot getPilot() {
@@ -74,78 +118,59 @@ public class Zbor {
         this.pilot = pilot;
     }
 
-    public Date getData_plecare() {
-        return data_plecare;
-    }
-    public Set<Client> getPasageri() {
-        return pasageri;
-    }
-    public void setData_plecare(Date data_plecare) {
-        this.data_plecare = data_plecare;
+    public Pilot getCopilot() {
+        return copilot;
     }
 
-    public void setPasageri(Set<Client> pasageri) {
-        this.pasageri = pasageri;
+    public void setCopilot(Pilot copilot) {
+        this.copilot = copilot;
     }
 
-    public void setDelay(int delay) {
-        this.delay = delay;
+    public CompanieAeriana getCompanie_aeriana() {
+        return companie_aeriana;
     }
 
-    public Date getData_sosire() {
-        return data_sosire;
+    public void setCompanie_aeriana(CompanieAeriana companie_aeriana) {
+        this.companie_aeriana = companie_aeriana;
     }
 
-    public void setData_sosire(Date data_sosire) {
-        this.data_sosire = data_sosire;
+    public Avion getAvion() {
+        return avion;
     }
 
-    public Zbor( Aeroport plecare, Aeroport sosire, Avion avion, Pilot pilot, Date data_plecare, Date data_sosire, int pret) {
-        this.plecare = plecare;
-        this.sosire = sosire;
+    public void setAvion(Avion avion) {
         this.avion = avion;
-        this.pilot = pilot;
-        this.data_plecare = data_plecare;
-        this.data_sosire = data_sosire;
-        this.pret=pret;
-        this.delay=0;
     }
 
-    @OneToOne
-    @JoinColumn(name = "plecare_id",referencedColumnName = "id_aeroport")
-    private Aeroport plecare;
-
-    @OneToOne
-    @JoinColumn(name = "sosire_id",referencedColumnName = "id_aeroport")
-    private Aeroport sosire;
-
-    @OneToOne
-    @JoinColumn(name = "avion_id",referencedColumnName = "id_avion")
-    private Avion avion;
-
-    @OneToOne
-    @JoinColumn(name = "pilot_id",referencedColumnName = "id_pilot")
-    private Pilot pilot;
-
-    public int getPret() {
-        return pret;
+    public Date getData_plecare_estimata() {
+        return data_plecare_estimata;
     }
 
-    @Column(nullable = false)
-    private Date data_plecare;
-
-    public int getDelay() {
-        return delay;
+    public void setData_plecare_estimata(Date data_plecare_estimata) {
+        this.data_plecare_estimata = data_plecare_estimata;
     }
 
-    @Column(nullable = false)
-    private Date data_sosire;
-
-    @Column(nullable = false)
-    private int pret;
-
-    public void addPasager(Client client){
-        pasageri.add(client);
+    public Date getData_sosire_estimata() {
+        return data_sosire_estimata;
     }
 
+    public void setData_sosire_estimata(Date data_sosire_estimata) {
+        this.data_sosire_estimata = data_sosire_estimata;
+    }
+
+    public Date getData_plecare_reala() {
+        return data_plecare_reala;
+    }
+
+    public void setData_plecare_reala(Date data_plecare_reala) {
+        this.data_plecare_reala = data_plecare_reala;
+    }
+
+    public Date getData_sosire_reala() {
+        return data_sosire_reala;
+    }
+
+    public void setData_sosire_reala(Date data_sosire_reala) {
+        this.data_sosire_reala = data_sosire_reala;
+    }
 }
